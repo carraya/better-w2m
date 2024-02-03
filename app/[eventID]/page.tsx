c721166cb556be238fcfbd4d64539793a9b0462e
Farouk Charkas <farouk.charkas@gmail.com>
2024-02-03 16:55:18 -0500

    Made first draft of UI

--- app/[eventID]/page.tsx
+++ app/[eventID]/page.tsx
@@ -1,3 +1,5 @@
+import { InputGrid } from "@/components/input-grid";
+import { Button } from "@/components/ui/button";
 import { getEventById } from "@/lib/api/event/queries";
 
 export default async function Event({
@@ -7,8 +9,22 @@ export default async function Event({
 }) {
   // const event = await getEventById(params.eventID);
   return (
-    <main>
-      <h1>Helo</h1>
+    <main className="flex flex-col min-h-screen p-[60px]">
+      <div className="pb-5 flex">
+        <h1 className="text-2xl">TechX Meeting</h1>
+        <div className="flex-1"></div>
+        <Button className="mr-2">Share Link</Button>
+        <Button className="bg-blue-500">Fill Out Availability</Button>
+      </div>
+      <div className="flex flex-1 gap-[60px]">
+        <div id="individual" className="flex-1 bg-red-300">
+          <h3 className="font-bold">Your Availability</h3>
+        </div>
+        <div id="group" className="flex flex-col flex-1 bg-green-300">
+          <h3 className="font-bold">Group's Availability</h3>
+          <InputGrid></InputGrid>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ components/input-grid.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function InputGrid() {
+  return (
+    <div className="flex flex-1 bg-white border-[1px] rounded-3xl">
+      <div></div>
+    </div>
+  );
+}
